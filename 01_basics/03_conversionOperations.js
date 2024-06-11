a1936907c1b1175de29b5a24b3180277a7249a40
Nishank Lalit Jain <85564014+NishankJain03@users.noreply.github.com>
2024-06-11 18:37:02 +0000

    conversion and comparison

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,54 @@
+let score = "33abc"
+let score1 = null
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abcc" => NaN (Not a Number)
+// true => 1
+// false => 0
+// "" => false
+// "nishank" => true
+
+// let isLoggedInNew = 1
+// let isLoggedInNew = ""
+let isLoggedInNew = "nishank"
+
+let changeisLoggedInNew = Boolean(isLoggedInNew)
+// console.log(changeisLoggedInNew)
+
+
+let someNumber = 33
+
+let newString = String(someNumber)
+// console.log(someNumber)
+// console.log(typeof newString)
+
+
+
+
+
+
+// Operations:
+
+
+let str1 = "nishank"
+let str2 = " jain"
+let str3 = str1 + str2
+console.log(str3);
+
+
+// But here the problem arise
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
+console.log(+true)
+console.log(+"");
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2  > 1);
+// console.log(2  >= 1);
+// console.log(2  != 1);
+// console.log(2  == 1);
+// console.log(2  === 1);
+// console.log(2  <=  1);
+
+
+// console.log("2" > 1)
+// console.log(null >= 0)
+// console.log(null == 0)
+
+// the above two console log gave different output because the equality and comparison works differently 
+// the comparison convert null => 0 then 0 >= 0 return true
+
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+
+// strict checking => ===    :=>  it checks value and datatype also
+console.log("2" == 2)
+console.log("2" === 2)
